3476b92f60122a6ed8f24167be54f038b2177798
Luciano P <lucianopetrate@gmail.com>
2022-09-21 22:07:14 -0300

    chart

---
+++ src/dummydata.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+      name: "Ene",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 2000,
+    },
+    {
+      name: "Abr",
+      "Active User": 2780,
+    },
+    {
+      name: "May",
+      "Active User": 1890,
+    },
+    {
+      name: "Jun",
+      "Active User": 2390,
+    },
+    {
+      name: "Jul",
+      "Active User": 3490,
+    },
+    {
+      name: "Ago",
+      "Active User": 8000,
+    },
+    {
+      name: "Sep",
+      "Active User": 9000,
+    },
+    {
+      name: "Oct",
+      "Active User": 7000,
+    },
+    {
+      name: "Nov",
+      "Active User": 2000,
+    },
+    {
+      name: "Dic",
+      "Active User": 5000,
+    },
+  ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../featuredInfo/FeaturedInfo'
+import {userData} from '../../dummydata.js'
 import './home.scss'
 
 function Home() {
   return (
     <div className='home'>
         <FeaturedInfo/>
+        <Chart data={userData} title='Estadisticas de Usuario' grid dataKey='Active User'/>
     </div>
   )
 }
